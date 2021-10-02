786e868ba48e8a0a5eaf21ef6f20742e2649c36d
stefkal <stefanoskalam@gmail.com>
2021-10-02 18:34:00 -0400

    Added Previous and Next Functionality

--- printer.py
+++ printer.py
@@ -10,6 +10,8 @@ import sys
 
 class BrowserDriver:
     def __init__(self):
+        self.url_list = []
+        self.current_url = 0
         # if user is on Windows
         if platform == "win32":
             options = webdriver.ChromeOptions()
@@ -29,19 +31,34 @@ class BrowserDriver:
             sys.exit()
 
     # generate a random URL, get it and screenshot the page
-    def save_image(self):
-        random_string = get_random_string(6)
-        url = "https://prnt.sc/" + random_string
+    def save_image(self, url):
         self.driver.get(url)
 
         image_div = self.driver.find_element_by_class_name("under-image")
         image_div.find_element_by_tag_name('img').screenshot('image.png')
 
+    def get_random_url(self):
+        random_string = get_random_string(6)
+        url = "https://prnt.sc/" + random_string
+        self.url_list.append(url)
+        self.current_url = len(self.url_list) - 1
+        return self.url_list[self.current_url]
+
+    def next_url(self):
+        if self.current_url != len(self.url_list) - 1:
+            self.current_url += 1
+        return self.url_list[self.current_url]
+
+    def prev_url(self):
+        if self.current_url != 0:
+            self.current_url -= 1
+        return self.url_list[self.current_url]
+
 
 class UI(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.nextImage = tk.Button(self)
+        self.prevImage = tk.Button(self)
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         self.master = master
         self.pack()
@@ -50,22 +67,35 @@ class UI(tk.Frame):
         self.create_widgets()
         self.quit.pack(side="bottom")
 
+    def random(self):
+        url = self.driver.get_random_url()
+        self.refresh_ui(url)
+
+    def next(self):
+        url = self.driver.next_url()
+        self.refresh_ui(url)
+
+    def previous(self):
+        url = self.driver.prev_url()
+        self.refresh_ui(url)
+
     # Creates next putton and adds listener to it
     def create_widgets(self):
-        self.nextImage["text"] = "Next"
-        self.nextImage.pack()
-        self.nextImage["command"] = self.refresh_ui
+        self.randomImage = tk.Button(self, text="Random", command=lambda: self.random()).pack()
+        self.nextImage = tk.Button(self, text="Next", command=lambda: self.next()).pack()
+        self.prevImage = tk.Button(self, text="Prev", command=lambda: self.previous()).pack()
 
     # saves, zooms and refreshes image on TKinter GUI
     def first_update(self):
-        self.driver.save_image()
+        url = self.driver.get_random_url()
+        self.driver.save_image(url)
         self.img = tk.PhotoImage(file="image.png")
         self.img = self.img.zoom(2)
         self.label = ttk.Label(image=self.img)
         self.label.pack()
 
-    def refresh_ui(self):
-        self.driver.save_image()
+    def refresh_ui(self, url):
+        self.driver.save_image(url)
         self.img = tk.PhotoImage(file="image.png")
         self.label['image'] = self.img
         self.label.photo_ref = self.img
